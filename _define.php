48298719cd22721a99019b1678c2b588911d6011
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:43:07 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,22 +10,25 @@
  * @copyright Franck Paul carnet.franck.paul@gmail.com
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "adaptiveImages",                                                               // Name
-    "Implements the 3-layers technique for Adaptive Images generation (by Nursit)", // Description
-    "Franck Paul and contributors",                                                 // Author
+    'adaptiveImages',                                                               // Name
+    'Implements the 3-layers technique for Adaptive Images generation (by Nursit)', // Description
+    'Franck Paul and contributors',                                                 // Author
     '0.9',                                                                          // Version
     [
         'requires'    => [['core', '2.16']],                               // Dependencies
         'permissions' => 'admin',                                          // Permissions
         'type'        => 'plugin',                                         // Type
-        'details'     => 'https://open-time.net/?q=adaptiveimages',        // Details URL
-        'support'     => 'https://github.com/franck-paul/adaptivesImages', // Support URL
         'settings'    => [                                                 // Settings
             'blog' => '#params.adaptiveimages_settings'
-        ]
+        ],
+
+        'details'    => 'https://open-time.net/?q=adaptivesImages',       // Details URL
+        'support'    => 'https://github.com/franck-paul/adaptivesImages', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/adaptivesImages/main/dcstore.xml'
     ]
 );
