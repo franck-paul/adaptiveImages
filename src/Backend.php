90709b4762c9820a0d5bb901e7eb53fd6c32eafa
franck <carnet.franck.paul@gmail.com>
2023-05-09 08:41:00 +0200

    Use My::checkContext()

--- src/Backend.php
+++ src/Backend.php
@@ -21,7 +21,7 @@ class Backend extends dcNsProcess
 {
     public static function init(): bool
     {
-        static::$init = defined('DC_CONTEXT_ADMIN');
+        static::$init = My::checkContext(My::BACKEND);
 
         // dead but useful code, in order to have translations
         __('adaptiveImages') . __('Implements the 3-layers technique for Adaptive Images generation (by Nursit)');
