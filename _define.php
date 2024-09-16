3ebb581f953b5c3dff5d49e9cc4c04db32b0bf26
franck <carnet.franck.paul@gmail.com>
2024-09-16 16:08:12 +0200

    New release 3.3

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'adaptiveImages',
     'Implements the 3-layers technique for Adaptive Images generation (by Nursit)',
     'Franck Paul and contributors',
-    '3.2',
+    '3.3',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
